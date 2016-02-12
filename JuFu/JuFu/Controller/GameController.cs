843ed0ce3002e9584088c35d88943747de4e8431
ChrisJu96 <christopher.junker@web.de>
2016-02-13 00:31:35 +0100

    Created GameController

---
+++ JuFu/JuFu/Controller/GameController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuFu.Controller
+{
+    class GameController
+    {
+        private int _pitchX;
+        private int _pitchY;
+
+        public GameController(string[] player, int gridX, int gridY)
+        {
+            _pitchX = gridX;
+            _pitchY = gridY;
+
+        }
+
+        public void SetPlayer()
+        {
+
+        }
+
+        public void SetMonster()
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void NextRound()
+        {
+
+        }
+
+        public void Checkfield()
+        {
+
+        }
+
+        public void CreatePitch()
+        {
+            
+        }
+    }
+}
