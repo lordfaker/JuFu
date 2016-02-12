38a1a8343cd4706d8d62ef16e4f08781ca78e9b8
Markus sommerschnee <markus.sommerschnee@outlook.de>
2016-02-13 00:34:35 +0100

    Add IMonster

---
+++ JuFu/JuFu/Monster/AbstractMonster.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuFu.Monster
+{
+    class AbstractMonster : IMonster
+    {
+        public void Move(string direction)
+        {
+            
+        }
+
+        public void Fight()
+        {
+            
+        }
+
+        public void Die()
+        {
+            
+        }
+    }
+}
---
+++ JuFu/JuFu/Monster/IMonster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuFu.Monster
+{
+    interface IMonster
+    {
+        void Move(string direction);
+
+        void Fight();
+
+        void Die();
+    }
+}
