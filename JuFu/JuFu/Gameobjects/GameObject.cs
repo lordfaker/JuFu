51fd8df0a703005e1cb0ee8420ece29bbc9cff0c
ChrisJu96 <christopher.junker@web.de>
2016-02-13 03:54:07 +0100

    Created GameObject

---
+++ JuFu/JuFu/Gameobjects/GameObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuFu.Gameobjects
+{
+    class GameObject
+    {
+    }
+}
