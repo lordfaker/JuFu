8a53c6c730b5976363aa213045ae5867f8c17300
Markus sommerschnee <markus.sommerschnee@outlook.de>
2016-02-13 03:02:39 +0100

    Add Field class

---
+++ JuFu/JuFu/Arena/Field.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuFu.Arena
+{
+    class Field
+    {
+        public bool IsSet { get; set; }
+        public Monster.Monster Monster { get; set; }
+    }
+}
