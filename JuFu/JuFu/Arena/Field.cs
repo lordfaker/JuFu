dd4758e58f152a54a0090f4bd766f43c01302f1f
Markus sommerschnee <markus.sommerschnee@outlook.de>
2016-02-13 04:33:16 +0100

    Fill pitch class with logic

--- JuFu/JuFu/Arena/Field.cs
+++ JuFu/JuFu/Arena/Field.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
 
 namespace JuFu.Arena
 {
@@ -11,10 +14,14 @@ namespace JuFu.Arena
         public readonly int Index;
         public bool IsSet { get; set; }
         public Monster.Monster Monster { get; set; }
-
+        private readonly Rectangle rectangle = new Rectangle();
+        
         public Field(int index)
         {
             Index = index;
+            rectangle.Fill = new SolidColorBrush(Colors.DarkSeaGreen);
+            rectangle.Width = 50;
+            rectangle.Height = 50;
         }
     }
 }
---
+++ JuFu/JuFu/Arena/Pitch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using JuFu.Controller;
+
+
+namespace JuFu.Arena
+{
+    class Pitch : Canvas
+    {
+        public Pitch() : base()
+        {
+            base.Width = 50*GameController.PITCH_X;
+            base.Height = 50;
+        }
+
+        public Pitch GetPitch()
+        {
+            Field[] pitch = new Field[GameController.PITCH_X];
+            for (int i = 0; i < pitch.Length; i++)
+            {
+                pitch[i] = new Field(i);
+            }
+
+            return this;
+        }
+    }
+}
